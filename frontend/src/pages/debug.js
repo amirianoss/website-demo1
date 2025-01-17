5cb0a03946aae4be16b77c759902242a8e563a3f
amirianoss <amirhosseinvalizadeh2000a@gmail.com>
2025-01-17 19:03:57 +0330

    Updates

--- frontend/src/pages/debug.js
+++
@@ -1,3 +0,0 @@
-// Add this temporarily to your Login component to debug
-console.log('Stored users:', localStorage.getItem('users'));
-console.log('Current user:', localStorage.getItem('currentUser'));
